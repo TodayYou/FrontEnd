5c917885fff5c94d5bbcb91a16ec2f8bab693bfc
jjuny-won <jjuny040627@gamil.com>
2024-07-07 21:43:45 +0900

    fix:도메인주소수정

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -5,7 +5,7 @@ let token = localStorage.getItem("accessToken");
 
 // axios 인스턴스 생성
 export const todayYouAxios = axios.create({
-  baseURL: "http://localhost:8080", // 포트 8080 설정
+  baseURL: "https://todayyouuu.com/api", // 포트 8080 설정
   headers: {
     Authorization: `Bearer ${token}`,
     "Content-Type": "application/json",
